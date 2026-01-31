41677658cc3978ec23c284e4b904108d34f9d00b
Kidus Berhane <124568569+Kidusbk@users.noreply.github.com>
2026-01-31 14:09:10 -0500

    Time: 217 ms (5.11%) | Memory: 12.7 MB (37.96%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,56 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # if s is None:
+        #     return True
+        # stack =[]
+        # for t in s:
+        #     if t == ')':
+        #         try:
+        #             current = stack.pop()
+        #             if current != '(':
+        #                 return False
+        #         except:
+        #             return False
+        #     elif t == '}':
+        #         try:
+        #             current = stack.pop()
+        #             if current != '{':
+        #                 return False
+        #         except:
+        #             return False
+        #     elif t == ']':
+        #         try:
+        #             current = stack.pop()
+        #             if current != '[':
+        #                 return False
+        #         except:
+        #             return False
+        #     else:
+        #         stack.append(t)
+        #     if len(stack) == 0:
+        #         return True
+        #     else:
+        #         return False
+
+        n = len(s)
+        if n == 0:
+            return True
+    
+        if n % 2 != 0:
+            return False
+    
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('{}', '').replace('()', '').replace('[]', '')
+    
+        if s == '':
+            return True
+        else:
+            return False
+                
+
+      
+                    
